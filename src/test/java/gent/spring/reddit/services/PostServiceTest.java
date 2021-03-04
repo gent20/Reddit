c46ff35daa984e6e0400b9f941362d715aa4ce19
gent20 <gentkastratio@gmail.com>
2021-03-04 16:51:32 +0100

    fixed a testing bug

--- src/test/java/gent/spring/reddit/services/PostServiceTest.java
+++ src/test/java/gent/spring/reddit/services/PostServiceTest.java
@@ -1,21 +1,29 @@
 package gent.spring.reddit.services;
 
 import gent.spring.reddit.Mapper.PostMapper;
+import gent.spring.reddit.dto.PostRequest;
 import gent.spring.reddit.dto.PostResponse;
 import gent.spring.reddit.model.Post;
+import gent.spring.reddit.model.Subreddit;
+import gent.spring.reddit.model.User;
 import gent.spring.reddit.repositories.PostRepository;
 import gent.spring.reddit.repositories.SubredditRepository;
 import gent.spring.reddit.repositories.UserRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
 import java.util.Optional;
+
+import static java.util.Collections.emptyList;
 
 @ExtendWith(MockitoExtension.class)
 class PostServiceTest {
@@ -31,15 +39,23 @@ class PostServiceTest {
     @Mock
     private PostMapper postMapper;
 
+    @Captor
+    private ArgumentCaptor<Post> postArgumentCaptor;
+
+    private PostService postService;
+
+    @BeforeEach
+    public void setup() {
+        postService = new PostService(postRepository, subredditRepository, userRepository, authService, postMapper);
+    }
+
     @Test
-    @DisplayName("Should find post by id")
-    void shouldFindPostById() {
-        PostService postService = new PostService(subredditRepository,postRepository,
-                userRepository, authService,postMapper);
-        Post post = new Post(123L,"First Post","http://url.site","Test",0,
-                null, Instant.now(),null);
+    @DisplayName("Should Retrieve Post by Id")
+    public void shouldFindPostById() {
+        Post post = new Post(123L, "First Post", "http://url.site", "Test",
+                0, null, Instant.now(), null);
         PostResponse expectedPostResponse = new PostResponse(123L, "First Post", "http://url.site", "Test",
-                "Test User", "Test Subredit", 0, 0, "1 Hour Ago");
+                "Test User", "Test Subredit", 0, 0, "1 Hour Ago", false, false);
 
         Mockito.when(postRepository.findById(123L)).thenReturn(Optional.of(post));
         Mockito.when(postMapper.mapToDto(Mockito.any(Post.class))).thenReturn(expectedPostResponse);
@@ -48,5 +64,28 @@ class PostServiceTest {
 
         Assertions.assertThat(actualPostResponse.getId()).isEqualTo(expectedPostResponse.getId());
         Assertions.assertThat(actualPostResponse.getPostName()).isEqualTo(expectedPostResponse.getPostName());
+    }
+
+    @Test
+    @DisplayName("Should Save Posts")
+    public void shouldSavePosts() {
+        User currentUser = new User(123L, "test user", "secret password", "user@email.com", Instant.now(), true);
+        Subreddit subreddit = new Subreddit(123L, "First Subreddit", "Subreddit Description", emptyList(), Instant.now(), currentUser);
+        Post post = new Post(123L, "First Post", "http://url.site", "Test",
+                0, null, Instant.now(), null);
+        PostRequest postRequest = new PostRequest(null, "First Subreddit", "First Post", "http://url.site", "Test");
+
+        Mockito.when(subredditRepository.findByName("First Subreddit"))
+                .thenReturn(Optional.of(subreddit));
+        Mockito.when(authService.getCurrentUser())
+                .thenReturn(currentUser);
+        Mockito.when(postMapper.map(postRequest, subreddit, currentUser))
+                .thenReturn(post);
+
+        postService.save(postRequest);
+        Mockito.verify(postRepository, Mockito.times(1)).save(postArgumentCaptor.capture());
+
+        Assertions.assertThat(postArgumentCaptor.getValue().getPostId()).isEqualTo(123L);
+        Assertions.assertThat(postArgumentCaptor.getValue().getPostName()).isEqualTo("First Post");
     }
 }
